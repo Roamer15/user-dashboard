d76216848c5890a998be81679347cfbe971858a7
Roamer15 <ianseeksbeta@gmail.com>
2025-01-25 14:24:34 +0100

    Form validation section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 // import React from 'react'
-import { useFormik } from 'formik'
+import { useFormik, Formik, Form, Field } from 'formik'
 import { signupSchema } from './schemas/indexScheme'
 
 function App() {
@@ -14,43 +14,51 @@ function App() {
   const {values, handleBlur, handleChange, handleSubmit, errors, touched} = useFormik({
     initialValues,
     validationSchema: signupSchema,
-    onSubmit: (values) => {
+    onSubmit: (values, actions) => {
       console.log(values)
+      actions.resetForm()
     }
   })
 
   console.log(touched)
 
+  const onSubmit = (values, actions) => {
+    console.log(values)
+    actions.resetForm()
+  }
+
   return (
     <>
       <div className="app">
-        <form className="signup_form" onSubmit={handleSubmit}>
-          <label htmlFor="name">Name</label>
-          <input type="text" name="name" value={values.name} onBlur={handleBlur} onChange={handleChange}/>
-          <div className="error-container">
-            {errors.name && touched.name && (<p className='form_error'>{errors.name}</p>)}
-          </div>
-
-          <label htmlFor="email">Email</label>
-          <input type="email" name="email" value={values.email} onBlur={handleBlur} onChange={handleChange}/>
-          <div className="error-container">
-            {errors.name && touched.name && (<p className='form_error'>{errors.name}</p>)}
-          </div>
-
-          <label htmlFor="password">Password</label>
-          <input type="password" name="password" value={values.password} onBlur={handleBlur} onChange={handleChange}/>
-          <div className="error-container">
-            {errors.name && touched.name && (<p className='form_error'>{errors.name}</p>)}
-          </div>
-
-          <label htmlFor="cpassword">Confirm Password</label>
-          <input type="password" name="cpassword" value={values.cpassword} onBlur={handleBlur} onChange={handleChange}/>
-          <div className="error-container">
-            {errors.name && touched.name && (<p className='form_error'>{errors.name}</p>)}
-          </div>
+        <Formik initialValues={initialValues} validationSchema={signupSchema} onSubmit={handleSubmit}>
+          {props => (
+            <Form className="signup_form">
+            <label htmlFor="name">Name</label>
+            <input type="text" name="name" value={values.name} onBlur={handleBlur} onChange={handleChange}/>
+            <Field type='text' name='name'/>
+            <div className="error-container">
+              {errors.name && touched.name && (<p className='form_error'>{errors.name}</p>)}
+            </div>
+            <label htmlFor="email">Email</label>
+            <input type="email" name="email" value={values.email} onBlur={handleBlur} onChange={handleChange}/>
+            <div className="error-container">
+              {errors.name && touched.name && (<p className='form_error'>{errors.name}</p>)}
+            </div>
+            <label htmlFor="password">Password</label>
+            <input type="password" name="password" value={values.password} onBlur={handleBlur} onChange={handleChange}/>
+            <div className="error-container">
+              {errors.name && touched.name && (<p className='form_error'>{errors.name}</p>)}
+            </div>
+            <label htmlFor="cpassword">Confirm Password</label>
+            <input type="password" name="cpassword" value={values.cpassword} onBlur={handleBlur} onChange={handleChange}/>
+            <div className="error-container">
+              {errors.name && touched.name && (<p className='form_error'>{errors.name}</p>)}
+            </div>
+            <button type='submit'>Submit</button>
+            </Form>
+          )}
           
-          <button type='submit'>Submit</button>
-          </form>
+        </Formik>
       </div>
     </>
   )
